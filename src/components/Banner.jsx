e71564661797c0158237c83e7760ab4dd9b13a03
myangmi <michael.yangmi@outlook.com>
2021-12-01 02:33:18 -0600

    using new IEX Sandbox endpoint

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Tickers from './Tickers.jsx';
 
 import './Banner.scss';
 
-const Banner = () => (
+const mockSymbols = ['AMD', 'MSFT', 'AMZN', 'INTC', 'TSM', 'AAPL', 'FB', 'NFLX']; // TODO: Implement dynamics symbol selection
+
+const Banner = () => {
+  const [quoteData, setQuoteData] = useState(mockSymbols);
+
+  useEffect(() => {
+    axios.get(
+      `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${mockSymbols}&token=Tpk_87a7db1434064012a5134a13fac6355d&types=price`,
+    ).then(
+      (res) => {
+        setQuoteData(res.data);
+      },
+    ).catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  return (
     <div className='Banner' >
-      <Tickers symbols={['AMD', 'MSFT', 'AMZN', 'INTC', 'TSM', 'AAPL', 'FB', 'NFLX']} />
+      <Tickers symbols={quoteData} />
     </div>
-);
+  );
+};
 
 export default Banner;
--- src/components/Tickers.jsx
+++ src/components/Tickers.jsx
@@ -1,36 +1,27 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 import PropTypes, { string } from 'prop-types';
-import axios from 'axios';
 
 import './Tickers.scss';
 
 const propTypes = {
-  symbols: PropTypes.arrayOf(string).isRequired,
+  quoteData: PropTypes.arrayOf(string),
 };
 
 const Tickers = ({ symbols }) => {
-  // TODO: Find better API
-  const getQuotes = (symbol) => {
-    // axios.get(
-    //   `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=2LG1M918AHBXRKAW`,
-    // ).then((res) => {
-    //   const latest = res.data['Meta Data']['3. Last Refreshed'];
-    //   console.log(res.data['Time Series (Daily)'][latest]['4. close']);
-    //   return res.data['Time Series (Daily)'][latest]['4. close'];
-    // }).catch((err) => {
-    //   console.log(err);
-    //   return '25';
-    // });
-
-    return 'placeholder';
-  };
-
   const content = (
     <>
       <ul className='Tickers'>
-        {symbols.map((ticker, i) => (<li key={i}>{ticker}: ${`${getQuotes(ticker)}`}</li>))}
-        {symbols.map((ticker, i) => (<li key={i}>{ticker}: ${`${getQuotes(ticker)}`}</li>))}
+        {
+          Object.keys(symbols).map((ticker, i) => (
+            <li key={i}>{`${ticker}`}: ${`${symbols[ticker].price}`}</li>
+          ))
+        }
+        {
+          Object.keys(symbols).map((ticker, i) => (
+            <li key={i}>{`${ticker}`}:  ${`${symbols[ticker].price}`}</li>
+          ))
+        }
       </ul>
     </>
   );
