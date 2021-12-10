212f87a883df7e6af0399be31f607c77802fcc3c
Michael Yangmi <michael.yangmi@outlook.com>
2021-12-09 20:39:34 -0600

    Finalized webpack and babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,82 @@
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let mode = 'development';
+let target = 'web';
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: './public/index.html',
+  }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist';
+}
+
+if (process.env.SERVE) {
+  plugins.push(new ReactRefreshWebpackPlugin());
+}
 
 module.exports = {
-  mode: 'production',
+  mode,
+
   entry: './src/index.jsx',
+
   output: {
-    path: path.resolve('build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
+
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '' },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
       },
     ],
   },
-  externals: {
-    react: 'react',
+
+  plugins,
+
+  target,
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    hot: true,
   },
 };
